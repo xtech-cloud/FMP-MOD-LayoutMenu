b030ed1d5a5c73305d789ceb4fe54078348fc5a6
Niall Liu <easlee@outlook.com>
2023-04-12 14:59:26 +0800

    update

--- unity2021/LayoutMenu/Assets/Scripts/Module/MyConfig.cs
+++ unity2021/LayoutMenu/Assets/Scripts/Module/MyConfig.cs
@@ -1,5 +1,6 @@
 
 using System.Xml.Serialization;
+using UnityEngine;
 
 namespace XTC.FMP.MOD.LayoutMenu.LIB.Unity
 {
@@ -8,10 +9,74 @@ namespace XTC.FMP.MOD.LayoutMenu.LIB.Unity
     /// </summary>
     public class MyConfig : MyConfigBase
     {
+        public class Border
+        {
+            [XmlAttribute("left")]
+            public int left { get; set; } = 0;
+            [XmlAttribute("right")]
+            public int right { get; set; } = 0;
+            [XmlAttribute("top")]
+            public int top { get; set; } = 0;
+            [XmlAttribute("bottom")]
+            public int bottom { get; set; } = 0;
+        }
+
+        public class Offset2
+        {
+            [XmlAttribute("x")]
+            public int x { get; set; } = 0;
+            [XmlAttribute("y")]
+            public int y { get; set; } = 0;
+        }
+
+        public class Label
+        {
+            [XmlAttribute("fontSize")]
+            public int fontSize { get; set; } = 12;
+            [XmlAttribute("offset")]
+            public int offset { get; set; } = 24;
+            [XmlAttribute("width")]
+            public int width { get; set; } = 100;
+            [XmlAttribute("height")]
+            public int height { get; set; } = 32;
+            [XmlAttribute("color")]
+            public string color { get; set; } = "#000000FF";
+        }
+
+        public class Cell
+        {
+            [XmlAttribute("width")]
+            public int width { get; set; } = 0;
+            [XmlAttribute("height")]
+            public int height { get; set; } = 0;
+            [XmlElement("Label")]
+            public Label label { get; set; } = new Label();
+        }
+
+        public class GridLayout
+        {
+            [XmlAttribute("column")]
+            public int column { get; set; } = 0;
+            [XmlAttribute("row")]
+            public int row { get; set; } = 0;
+            [XmlElement("Anchor")]
+            public Anchor anchor { get; set; } = new Anchor();
+            [XmlElement("Padding")]
+            public Border padding { get; set; } = new Border();
+            [XmlElement("Spacing")]
+            public Offset2 spacing { get; set; } = new Offset2();
+        }
+
         public class Style
         {
             [XmlAttribute("name")]
             public string name { get; set; } = "";
+            [XmlAttribute("layout")]
+            public string layout { get; set; } = "";
+            [XmlElement("GridLayout")]
+            public GridLayout gridLayout { get; set; } = new GridLayout();
+            [XmlElement("Cell")]
+            public Cell cell { get; set; } = new Cell();
         }
 
 
--- unity2021/LayoutMenu/Assets/Scripts/Module/MyInstance.cs
+++ unity2021/LayoutMenu/Assets/Scripts/Module/MyInstance.cs
@@ -7,6 +7,12 @@ using UnityEngine.UI;
 using LibMVCS = XTC.FMP.LIB.MVCS;
 using XTC.FMP.MOD.LayoutMenu.LIB.Proto;
 using XTC.FMP.MOD.LayoutMenu.LIB.MVCS;
+using Unity.VisualScripting.Dependencies.NCalc;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
+using System;
+using Unity.Collections.LowLevel.Unsafe;
 
 namespace XTC.FMP.MOD.LayoutMenu.LIB.Unity
 {
@@ -15,6 +21,15 @@ namespace XTC.FMP.MOD.LayoutMenu.LIB.Unity
     /// </summary>
     public class MyInstance : MyInstanceBase
     {
+        public class UiReference
+        {
+            public RectTransform rtPanel;
+            public GameObject templateCell;
+        }
+
+        private UiReference uiReference_ = new UiReference();
+        private ContentReader contentReader_ = null;
+        private List<string> contentUriS_ = new List<string>();
 
         public MyInstance(string _uid, string _style, MyConfig _config, MyCatalog _catalog, LibMVCS.Logger _logger, Dictionary<string, LibMVCS.Any> _settings, MyEntryBase _entry, MonoBehaviour _mono, GameObject _rootAttachments)
             : base(_uid, _style, _config, _catalog, _logger, _settings, _entry, _mono, _rootAttachments)
@@ -29,6 +44,22 @@ namespace XTC.FMP.MOD.LayoutMenu.LIB.Unity
         /// </remarks>
         public void HandleCreated()
         {
+            uiReference_.rtPanel = rootUI.transform.Find("Panel").GetComponent<RectTransform>();
+            uiReference_.templateCell = rootUI.transform.Find("Panel/Cell").gameObject;
+            uiReference_.templateCell.SetActive(false);
+            // 应用节点文字样式
+            {
+
+                var cellText = rootUI.transform.Find("Panel/Cell/text").GetComponent<RectTransform>();
+                cellText.sizeDelta = new Vector2(style_.cell.label.width, style_.cell.label.height);
+                cellText.anchoredPosition = new Vector2(0, -style_.cell.label.offset);
+                var text = cellText.GetComponent<Text>();
+                text.fontSize = style_.cell.label.fontSize;
+                Color color = Color.black;
+                ColorUtility.TryParseHtmlString(style_.cell.label.color, out color);
+                text.color = color;
+            }
+            buildLayout();
         }
 
         /// <summary>
@@ -46,6 +77,9 @@ namespace XTC.FMP.MOD.LayoutMenu.LIB.Unity
         /// </remarks>
         public void HandleOpened(string _source, string _uri)
         {
+            contentReader_ = new ContentReader(contentObjectsPool);
+            contentReader_.AssetRootPath = settings_["path.assets"].AsString();
+            fillCells();
             rootUI.gameObject.SetActive(true);
             rootWorld.gameObject.SetActive(true);
         }
@@ -57,6 +91,112 @@ namespace XTC.FMP.MOD.LayoutMenu.LIB.Unity
         {
             rootUI.gameObject.SetActive(false);
             rootWorld.gameObject.SetActive(false);
+            contentReader_ = null;
+        }
+
+        private void buildLayout()
+        {
+            if (style_.layout == "GridLayout")
+            {
+                alignByAncor(uiReference_.rtPanel.transform, style_.gridLayout.anchor);
+                var layout = uiReference_.rtPanel.gameObject.AddComponent<GridLayoutGroup>();
+                layout.cellSize = new Vector2(style_.cell.width, style_.cell.height);
+                layout.padding = new RectOffset(style_.gridLayout.padding.left, style_.gridLayout.padding.right, style_.gridLayout.padding.left, style_.gridLayout.padding.bottom);
+                layout.spacing = new Vector2(style_.gridLayout.spacing.x, style_.gridLayout.spacing.y);
+                layout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+                layout.constraintCount = style_.gridLayout.column;
+            }
+        }
+
+        private void fillCells()
+        {
+            // 创建节点的函数定义
+            Action<string> createCell = (_contentUri) =>
+            {
+                var clone = GameObject.Instantiate(uiReference_.templateCell, uiReference_.templateCell.transform.parent);
+                clone.name = _contentUri;
+                clone.gameObject.SetActive(true);
+                contentReader_.ContentUri = _contentUri;
+                contentReader_.LoadText("meta.json", (_bytes) =>
+                {
+                    try
+                    {
+                        string json = Encoding.UTF8.GetString(_bytes);
+                        var contentSchema = JsonConvert.DeserializeObject<ContentMetaSchema>(json);
+                        clone.transform.Find("text").GetComponent<Text>().text = contentSchema.name;
+                    }
+                    catch (Exception ex)
+                    {
+                        logger_.Error("load:{0}/meta.json throw exception", _contentUri);
+                        logger_.Exception(ex);
+                    }
+                }, () => { });
+                contentReader_.LoadTexture("icon.png", (_texture) =>
+                {
+                    clone.GetComponent<RawImage>().texture = _texture;
+                }, () => { });
+            };
+
+            // 开始解析包含的包
+            Dictionary<string, List<string>> bundleUuidS = new Dictionary<string, List<string>>();
+            foreach (var section in catalog_.sectionS)
+            {
+                if (!new List<string>(section.instanceS).Contains(uid))
+                    continue;
+                foreach (var content in section.contentS)
+                {
+                    var valS = content.Split("/");
+                    if (valS.Length != 2)
+                        continue;
+                    if (!bundleUuidS.ContainsKey(valS[0]))
+                        bundleUuidS.Add(valS[0], new List<string>());
+                    if (!bundleUuidS[valS[0]].Contains(content))
+                        bundleUuidS[valS[0]].Add(content);
+                }
+            }
+
+            // 包加载的计数器序列
+            CounterSequence bundleLoadSequence = new CounterSequence(bundleUuidS.Count);
+            bundleLoadSequence.OnFinish = () =>
+            {
+                logger_.Info("found [{0}] contents", contentUriS_.Count);
+                foreach (var contentUri in contentUriS_)
+                {
+                    createCell(contentUri);
+                }
+            };
+
+            // 开始解析匹配的内容路径
+            foreach (var bundleUuid in bundleUuidS.Keys)
+            {
+                contentReader_.ContentUri = bundleUuid;
+                contentReader_.LoadText("meta.json", (_bytes) =>
+                {
+                    try
+                    {
+                        var json = Encoding.UTF8.GetString(_bytes);
+                        var bundle = JsonConvert.DeserializeObject<BundleMetaSchema>(json);
+                        foreach (var contentUuid in bundle.foreign_content_uuidS)
+                        {
+                            string contentUri = string.Format("{0}/{1}", bundleUuid, contentUuid);
+                            var contentPatternS = bundleUuidS[bundleUuid];
+                            foreach (var pattern in contentPatternS)
+                            {
+                                if (BundleMetaSchema.IsMatch(contentUri, pattern))
+                                {
+                                    contentUriS_.Add(contentUri);
+                                }
+                            }
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        logger_.Error("load:{0}/meta.json throw exception", bundleUuid);
+                        logger_.Exception(ex);
+                    }
+                    bundleLoadSequence.Tick();
+                }, () => { });
+            }
         }
     }
 }
